b098c40bcd1855bcfab6a0eefa952ca6dfe658cb
wangchengbing329 <319264106@qq.com>
2019-06-17 23:22:47 +0800

    大项目如何开始和mpvue

---
+++ Vue/element-starter/src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import {
+    login
+} from 'pages/'
+// this.$router this.$route
+Vue.use(VueRouter);
+const routes = [{
+    path:'/',
+    name:'登录',
+    hidden:'true',
+    component:login
+}];
+export default new VueRouter({
+routes,
+// stric:process.env.NODE_ENV !=='production'
+})
