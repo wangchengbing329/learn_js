dee82976eb7131b1e819f0b2cb6603421bb25715
wangchengbing329 <319264106@qq.com>
2019-06-28 14:21:39 +0800

    typescript-router

---
+++ React/react-typescript-router/src/api/member/mockData.ts
@@ -0,0 +1,13 @@
+import { MemberEntity} from '../../model'
+export const members:MemberEntity[] = [
+    {
+        id:123131,
+        login:'abfkf',
+        avatar_url:"https://avatars.githubusercontent.com/u/1457912?v=3"
+    },
+    {
+        id:1235451,
+        login:'ab4f',
+        avatar_url:"https://avatars.githubusercontent.com/u/4374977?v=3"
+    }
+]
---
+++ React/react-typescript-router/src/app.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+export const App:React.StatelessComponent<{}>=(props)=>{
+return(
+    <header></header>
+)
+}
