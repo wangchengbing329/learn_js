dcf5936d154b17034fd48819ef17a6307b3be98a
wangchengbing329 <319264106@qq.com>
2019-07-02 10:14:28 +0800

    4545

--- !Program/小米Lite/pages/cart/cart.js
+++ !Program/小米Lite/pages/cart/cart.js
@@ -5,138 +5,105 @@ Page({
    * 页面的初始数据
    */
   data: {
-hasList:false,
-carts:[  { id :"rk20",selected:true,url:'https://i1.mifile.cn/f/i/g/2015/cn-index/mi9-80.png?width=80&height=80',num:1,price:100,attr:'Redmi K20 Pro 8GB+256GB 火焰红'},
-{selected:true,url:'https://i1.mifile.cn/f/i/g/2015/cn-index/m8se-80.png?width=80&height=80',num:1,price:9900,attr:'Redmi K20 Pro 8GB+256GB 火焰红'},
-{selected:true,url:'https://i1.mifile.cn/f/i/g/2015/cn-index/m8se-80.png?width=80&height=80',num:1,price:9900,attr:'Redmi K20 Pro 8GB+256GB 火焰红'}
+    hasList: false,
+    carts: [{ id: "rk20", selected: true, url: 'https://i1.mifile.cn/f/i/g/2015/cn-index/mi9-80.png?width=80&height=80', num: 1, price: 100, attr: 'Redmi K20 Pro 8GB+256GB 火焰红' },
+    { selected: true, url: 'https://i1.mifile.cn/f/i/g/2015/cn-index/m8se-80.png?width=80&height=80', num: 1, price: 9900, attr: 'Redmi K20 Pro 8GB+256GB 火焰红' },
+    { selected: true, url: 'https://i1.mifile.cn/f/i/g/2015/cn-index/m8se-80.png?width=80&height=80', num: 1, price: 9900, attr: 'Redmi K20 Pro 8GB+256GB 火焰红' }
 
-  ],
-selectAll:true,
-totalPrice:0,
-totalGoods:0
+    ],
+    selectAll: true,
+    totalPrice: 0,
+    totalGoods: 0
   },
-goToArround(){
- wx.switchTab({
-   url: '../index/index',
-   
- });
-   
-    
-}
-,
-change(e){
-  console.log(e)
-  
-  let index = e.currentTarget.dataset.index;
-  let carts = this.data.carts
-  let selectAll = this.data.selectAll
-let selected = carts[index].selected;
-carts[index].selected = !selected; 
-// 只要一个取消就全选取消 
+  goToArround() {
+    wx.switchTab({
+      url: '../index/index',
 
-  const check = carts.some(ch=>{
-   
-    return ch.selected  === false;
-  })
-  if(check){
-    this.setData({
-      selectAll:false
-    })
-    
-  }else{
-    this.setData({
-      selectAll:true
-    })
+    });
+
+
   }
-  
-this.setData({
-  carts,
-  // selectAll:this.data.selectAll
+  ,
+  change(e) {
+    console.log(e)
 
-})
+    let index = e.currentTarget.dataset.index;
+    let carts = this.data.carts
+    let selectAll = this.data.selectAll
+    let selected = carts[index].selected;
+    carts[index].selected = !selected;
+    // 只要一个取消就全选取消 
+
+    const check = carts.some(ch => {
+
+      return ch.selected === false;
+    })
+    if (check) {
+      this.setData({
+        selectAll: false
+      })
+
+    } else {
+      this.setData({
+        selectAll: true
+      })
+    }
+
+    this.setData({
+      carts,
+      // selectAll:this.data.selectAll
+
+    })
 
 
 
-this.accountPrice()
-},
-selectAllStatus(){
-// console.log(e)
-let selectAll = this.data.selectAll;
-selectAll = !selectAll;
-let carts = this.data.carts;
-for( let i=0;i<carts.length;i++){
-  carts[i].selected = selectAll;
- 
-}
+    this.accountPrice()
+  },
+  selectAllStatus() {
+    // console.log(e)
+    let selectAll = this.data.selectAll;
+    selectAll = !selectAll;
+    let carts = this.data.carts;
+    for (let i = 0; i < carts.length; i++) {
+      carts[i].selected = selectAll;
 
-this.setData({
-  selectAll,
-  carts
-})
-this.accountPrice()
-},
-accountPrice(){
-  // console.log(e)
-// let num = this.data.totalGoods;
-let carts = this.data.carts;
-let goods = 0;
-let Price = 0;
-for(let i =0;i<carts.length;i++){
-  if(carts[i].selected === true){
-    Price += carts[i].num * carts[i].price  
-    goods += carts[i].num
-  }
-  
-}
+    }
 
-  
-this.setData({
-  totalGoods:goods,
-  totalPrice:Price
-  
+    this.setData({
+      selectAll,
+      carts
+    })
+    this.accountPrice()
+  },
+  accountPrice() {
+    // console.log(e)
+    // let num = this.data.totalGoods;
+    let carts = this.data.carts;
+    let goods = 0;
+    let Price = 0;
+    for (let i = 0; i < carts.length; i++) {
+      if (carts[i].selected === true) {
+        Price += carts[i].num * carts[i].price
+        goods += carts[i].num
+      }
 
-})
-// this.getTotal
-},
+    }
+
+
+    this.setData({
+      totalGoods: goods,
+      totalPrice: Price
+
+
+    })
+    // this.getTotal
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-// console.log(options)
-wx.getStorage({
-  key: 'amount',
-  success: (res) => {
-   let carts =  this.data.carts
-   let num =0;
-    let curNum = 0;
-    for(let i = 0; i<carts.length;i++){
-
-    
-    
-      
-      
-     if( carts[i].id === res.data[1]){
-  
-         num = carts[i].num; 
-         curNum = res.data[0]
-         
-         console.log(num)
-       this.setData({
-         num:curNum
-        
-       })
-       
-        this.accountPrice()
-
-      //  console.log(num)
-     } 
-    
-     }
-  },
-  fail: () => {},
-  complete: () => {}
-});
-  
+    // console.log(options)
+   
   },
 
   /**
@@ -149,23 +116,50 @@ wx.getStorage({
   /**
    * 生命周期函数--监听页面显示
    */
-  
+
   onShow: function () {
-   
-    
-    
-    setTimeout(()=>{
+
+    wx.getStorage({
+      key: 'amount',
+      success: (res) => {
+        let carts = this.data.carts
+        let num = 0;
+        let curNum = 0;
+        for (let i = 0; i < carts.length; i++) {
+          if (carts[i].id === res.data[1]) {
+          
+            num = 'carts[' + i +'].num';
+            curNum = res.data[0]
+            console.log(curNum ,i)
+            this.setData({
+              [num]: curNum
+
+            })
+
+            // this.accountPrice()
+
+            //  console.log(num)
+          }
+
+        }
+      },
+      fail: () => { },
+      complete: () => { }
+    });
+
+
+    setTimeout(() => {
 
       this.setData({
-        hasList:true,
-        
+        hasList: true,
+
       })
-      
+
       this.accountPrice()
-    },1000)
+    }, 1000)
 
   },
-  
+
   /**
    * 生命周期函数--监听页面隐藏
    */
@@ -177,7 +171,7 @@ wx.getStorage({
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-   
+
   },
 
   /**
