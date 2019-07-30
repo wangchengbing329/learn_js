660623cec390f25810cb5e114461b774bc87c0d0
wangchengbing329 <319264106@qq.com>
2019-07-30 22:14:08 +0800

    parogram updateb

--- react-program/geekbang/src/page/Courses/Courses.jsx
+++ react-program/geekbang/src/page/Courses/Courses.jsx
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Link,Route} from 'react-router-dom';
+import axios from 'axios';
 class Courses extends Component {
-    state = {  }
+    state = { 
+        nav:[]
+     }
+     componentDidMount(){
+         const nav = this.state.nav;
+        axios.get('https://www.easy-mock.com/mock/5ca457ff4767c3737055c8c2/example/geekbang/course')
+        .then(res=>{
+            this.setState({
+                nav:res.data.data.nav
+            })
+        })
+     }
     render() { 
+        const {nav} = this.state
         return (  
-            <div>6</div>
+            <div className="course">
+   <h1 style={{margin:"0.2rem 1rem"}}>讲堂</h1> 
+   <div className="link-wrapper">
+  {nav.map(item=>{
+      return (
+<div className="link" key={item.id}>
+    <Link path={'/courses/' + item.id}>
+        <img src={item.icon} alt="" className="course-icon"/>
+        <div className="course-name">{item.name}</div>
+    </Link>
+</div>
+      )
+  })}
+   </div>
+
+
+            </div>
         );
     }
 }
